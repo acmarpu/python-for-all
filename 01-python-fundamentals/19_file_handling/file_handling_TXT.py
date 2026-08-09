fd0c8364977d1c1d20246139b0561ac83c23489d
acmarpu <acmarpu@gmail.com>
2026-08-09 12:41:46 +0530

    #Updated:08092026:12:41

--- 01-python-fundamentals/19_file_handling/file_handling_TXT.py
+++ 01-python-fundamentals/19_file_handling/file_handling_TXT.py
@@ -1,12 +1,16 @@
-#  Read/write files from sample.txt
+import os
+print(os.getcwd())  # Get the current working directory
+
+#  this block is creating a new file and writing to it
 with open("sample.txt","w") as file:
     file.write("Hello, World!")
- 
+    file.write("Hello, Python!")
 
-# Append to files
+
+# Append to files to existing file
 with open("sample.txt","a") as file:
     file.write("\nThis is an additional line.")
-
+    file.write("\nThis is an additional line.")
 
 #  Read files
 with open("sample.txt","r") as file:
